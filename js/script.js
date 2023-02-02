c17c59ee2ade7573999ef734ed6103d386430629
Seweryn Nurczyk <snurczyk99@gmail.com>
2023-02-02 16:04:35 +0100

    Add 2 moves to game

--- js/script.js
+++ js/script.js
@@ -7,3 +7,10 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+ 
+let playerMove = `papier` ;
+
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to Kamień, to przegrywasz!');
