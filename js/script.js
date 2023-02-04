666266c7e5fa9bd77f43ce6595ee0ae4b768c5db
Seweryn Nurczyk <snurczyk99@gmail.com>
2023-02-04 14:18:15 +0100

    Add 3 players move to js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'Kamień';
 }
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+if(playerInput == '2'){
+    playerMove = 'Papier';
 }
-let computerMove = `kamień`;
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
- 
-let playerMove = `papier` ;
-
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to Kamień, to przegrywasz!');
+if(playerInput == '3'){
+    playerMove = 'Nożyce';
+}
+printMessage('Twój ruch to: ' + playerMove);
