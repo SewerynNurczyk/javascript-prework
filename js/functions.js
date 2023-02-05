1327d52ad88c101ec28261f535d3d5b4078776ea
Seweryn Nurczyk <snurczyk99@gmail.com>
2023-02-05 17:58:00 +0100

    Add functions js

--- js/functions.js
+++ js/functions.js
@@ -14,3 +14,39 @@ function getMoveName(argMoveId){
 		return 'Nożyce';
 	}
 }
+	function displayResult (argComputerMove,argPlayerMove){
+		if (argComputerMove == argPlayerMove) {
+			printMessage('Remis');
+			if( computerMove == 'Kamień' && playerMove == 'Papier'){
+				printMessage('Ty wygrywasz!');
+			}
+			if( computerMove == 'Kamień' && playerMove == 'Nożyce'){
+				printMessage('Komputer wygrywa!');
+			}
+			if( computerMove == 'Kamień' && playerMove == 'Kamień'){
+				printMessage('Mamy remis!');
+			}
+			if( computerMove == 'Papier' && playerMove == 'Nożyce'){
+				printMessage('Ty wygrywasz!');
+			}
+			if( computerMove == 'Papier' && playerMove == 'Kamień'){
+				printMessage('Komputer wygrywa!');
+			}
+			if( computerMove == 'Papier' && playerMove == 'Papier'){
+				printMessage('Mamy remis!');
+			}
+			if( computerMove == 'Nożyce' && playerMove == 'Papier'){
+				printMessage('Komputer wygrywa!');
+			}
+			if( computerMove == 'Nożyce' && playerMove == 'Kamień'){
+				printMessage('Ty wygrywasz!');
+			}
+			if( computerMove == 'Nożyce' && playerMove == 'Nożyce'){
+				printMessage('Mamy remis!');
+			}
+			
+			if(playerInput !='1' && playerInput !='2' && playerInput !='3' ){
+				printMessage ('Niewłaściwy ruch!')
+			}		
+	}
+	}
